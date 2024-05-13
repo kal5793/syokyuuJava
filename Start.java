6f54b52b2d35d8661382d22807073cd204e18788
Hikaru <zeal23310178@fsgcl.jp>
2024-05-13 12:14:43 +0900

    gitignoreコミット

---
+++ Start.java
@@ -0,0 +1,11 @@
+public class Start 
+{
+	public static void main(String[] args) 
+	{
+		MyFrame frame1=new RectFrame();
+		frame1.setLocation(200,100);
+		frame1.setLocation(150,150);
+		MyFrame frame2=new RectFrame();
+		frame2.setLocation(350,200);
+	}
+}
