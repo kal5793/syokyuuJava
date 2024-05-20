96d52f2373cdabf6177e00e9cea8df59b19161dc
Hikaru <zeal23310178@fsgcl.jp>
2024-05-20 10:20:04 +0900

    練習問題3-1のコミット

---
+++ 初級Java2.5/src/RectAndOval.java
@@ -0,0 +1,11 @@
+
+public class RectAndOval extends MyFrame
+{
+	public void run() 
+	{
+		fillRect(70,100,100,50);
+		fillRect(50,130,140,30);
+		fillOval(75,155,20,20);
+		fillOval(140,155,20,20);
+	}
+}
