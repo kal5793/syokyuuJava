36163a719a6cc9fb4dc99dfd7e417efc5cd6f939
Hikaru <zeal23310178@fsgcl.jp>
2024-05-20 11:09:33 +0900

    練習問題4-2のコミット

---
+++ 初級Java2.5/src/ManyFigures.java
@@ -0,0 +1,17 @@
+
+public class ManyFigures extends MyFrame 
+{
+	public void run() 
+	{
+		setColor(0,128,0);
+		int x=30;
+		fillRect(x,80,10,100);
+		fillRect(50,80,10,100);
+		fillRect(70,80,10,100);
+		fillRect(90,80,10,100);
+		fillRect(110,80,10,100);
+		fillRect(130,80,10,100);
+		fillRect(150,80,10,100);
+		fillRect(170,80,10,100);
+	}
+}
