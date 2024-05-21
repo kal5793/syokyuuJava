b03bb7292c4faf0c858c9c7d901940bb9434738c
Hikaru <zeal23310178@fsgcl.jp>
2024-05-21 11:54:48 +0900

    教科書5ｰ1のコミット

---
+++ 初級Java2.5/src/DisplayRectsSlow.java
@@ -0,0 +1,15 @@
+
+public class DisplayRectsSlow extends MyFrame
+{
+	public void run() 
+	{
+		setColor(0,128,0);
+		int x=30;
+		while(x<=170) 
+		{
+			fillRect(x,x,10,100);
+			x+=20;
+			sleep(1);
+		}
+	}
+}
