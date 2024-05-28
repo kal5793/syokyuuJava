81121f9f481bbd89fa402519df02d9cb317f6b68
Hikaru <zeal23310178@fsgcl.jp>
2024-05-28 10:18:42 +0900

    教科書6-3のコミット

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -4,12 +4,18 @@ public class DisplayFaceObject extends MyFrame
 	public void run() 
 	{
 		Face face1=new Face(50,50);
-		face1.draw(this);
+		Face face2=new Face(200,100);
+		for(int i=0;i<30;i++) 
+		{
+			clear();
+			face1.draw(this);
+			face2.draw(this);
+			face1.x+=10;
+			face1.y+=5;
+			face2.x-=10;
+			face2.y-=5;
+			sleep(0.1);
+		}
 		
-		Face face2=new Face(150,100);
-		face2.draw(this);
-		
-		Face face3=new Face(250,50);
-		face3.draw(this);
 	}
 }
