068155366ec9976c4254c4a0f1194d370ba55e69
Hikaru <zeal23310178@fsgcl.jp>
2024-06-10 11:15:49 +0900

    教科書8-3のコミット

--- 初級Java2.5/src/car.java
+++ 初級Java2.5/src/car.java
@@ -1,14 +1,13 @@
 
-public class car
+public class car extends Vehicle
 {
-	int x,y,vx,vy;
-	public car(int x,int y,int vx,int vy) 
-	{
-		this.x=x;
-		this.y=y;
-		this.vx=vx;
-		this.vy=vy;
+	
+	
+	public car(int x, int y, int vx, int vy) {
+		super(x, y, vx, vy);
+		// TODO 自動生成されたコンストラクター・スタブ
 	}
+
 	public void draw(MyFrame frame) 
 	{
 		frame.fillOval(x+10,y+40,20,20);
@@ -16,9 +15,5 @@ public class car
 		frame.fillRect(x,y+20,80,20);
 		frame.fillRect(x+20,y,40,20);
 	}
-	public void move() 
-	{
-		x+=vx;
-		y+=vy;
-	}
+	
 }
